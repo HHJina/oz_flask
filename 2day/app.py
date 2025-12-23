2743c8f9ea384475cc180a40bbc61286324595e6
HHJina <102576347+HHJina@users.noreply.github.com>
2025-12-23 10:08:36 +0900

    Update app.py

--- 2day/app.py
+++ 2day/app.py
@@ -11,6 +11,7 @@ app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+
 api = Api(app)
 api.register_blueprint(book_blp)
 
